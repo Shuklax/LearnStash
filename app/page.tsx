b38062883e3e018faeee09b41957e445cf27d2e6
Shuklax <harshshuklaknp@gmail.com>
2023-07-05 14:48:00 +0530

    implements UI for popular companions

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,45 @@
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to LearnStash</h1>
+    <main>
+      <h1>popular Companions</h1>
 
-        <Button>
-            Lets get started
-        </Button>
-    </div>
+        <section className="home-section">
+            <CompanionCard
+                id="1"
+                subject="science"
+                name= "Neura the Brainy Explorer"
+                topic= "Neural Network of the Brain"
+                duration= {45}
+                color= "#FFDA6E"
+            />
+            <CompanionCard
+                id="2"
+                subject= "Maths"
+                name= "Countsy the Number Wizard"
+                topic= "Derivatives & Integrals"
+                duration= {30}
+                color= "#E5D0FF"
+            />
+            <CompanionCard
+                id= "3"
+                subject= "language"
+                name= "Verba the Vocabulary Builder"
+                topic= "English Literature"
+                duration= {30}
+                color= "#BDE7FF"
+            />
+        </section>
+
+        <section className="home-section">
+            <CompanionsList/>
+            <CTA/>
+        </section>
+    </main>
   )
 }
 
