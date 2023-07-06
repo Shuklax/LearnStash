4eaf42725c6e70fdb4723e4bd910a13301b631ee
Shuklax <harshshuklaknp@gmail.com>
2023-07-06 18:29:07 +0530

    implements types for companion creation

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -1,8 +1,45 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableRow,
+    TableHeader,
+} from "@/components/ui/table";
 
+interface CompanionsListProps {
+    title:string;
+    companions:Companion[];
+    classNames?:string;
+}
 
-const CompanionsList = () => {
+const CompanionsList = ({title, companions, classNames}: CompanionsListProps) => {
     return (
-        <div>CompanionsList</div>
+        <article>
+            <h2>Recent Sessions</h2>
+
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Invoice</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+
+        </article>
     )
 }
 export default CompanionsList
